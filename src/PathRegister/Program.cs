565430d115f44e3db5b72b4dee7360c52ba6008e
yamatora <yt@yamadataiga.com>
2022-09-12 00:52:39 +0900

    - implement register/deregister path - add app.manifest

--- src/PathRegister/Program.cs
+++ src/PathRegister/Program.cs
@@ -1,9 +1,29 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+
+using libymtr.Windows;
+using Registry = libymtr.Windows.Registry;
 
 namespace PathRegister {
     internal class Program {
+        private const string OPT_DELETE = "--delete";
+
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            string path = System.Environment.CurrentDirectory;
+
+            if (args.Contains(OPT_DELETE)) {
+                DeregisterPath(path);   //  delete
+                return;
+            }
+            RegisterPath(path);     //  add
+        }
+
+        private static void RegisterPath(string path) {
+            Registry.Environment.AddPath(path);
+        }
+        private static void DeregisterPath(string path) {
+            Registry.Environment.RemovePath(path);
         }
     }
 }
